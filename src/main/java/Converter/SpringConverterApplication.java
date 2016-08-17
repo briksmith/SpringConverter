40e0b6dd0006486dbb505d3a8e2ebbfc11a430a4
Your Name <briksmith@earthlink.net>
2016-08-17 16:25:25 -0500

    test commit

--- src/main/java/Converter/SpringConverterApplication.java
+++ src/main/java/Converter/SpringConverterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringConverterApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConverterApplication.class, args);
 	}
