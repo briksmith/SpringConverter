b523f7d4f1a9ee43f288c2c6d771c7f67e7ed9db
Your Name <briksmith@earthlink.net>
2016-08-17 16:11:22 -0500

    init commit

---
+++ src/main/java/Converter/SpringConverterApplication.java
@@ -0,0 +1,12 @@
+package Converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConverterApplication.class, args);
+	}
+}
---
+++ src/test/java/Converter/SpringConverterApplicationTests.java
@@ -0,0 +1,16 @@
+package Converter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringConverterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
